04723278a49372e105fdeed0ec6109dd00e3c2c6
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-10-22 20:31:50 +0300

    http

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,7 +7,7 @@ import icon from "../images/emoji.svg";
 import styles from "../styles/Chat.module.css";
 import Messages from "./Messages";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://online-chut.onrender.com");
 
 const Chat = () => {
   const navigate = useNavigate()
